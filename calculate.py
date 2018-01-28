dba0e50b5c7018c0578a93ba799334bf9161df54
Federico <fes@foryouandyourcustomers.com>
2018-01-28 09:35:03 +0100

    adds year calculatoin

--- calculate.py
+++ calculate.py
@@ -39,22 +39,58 @@ def get_player(total_result, player_key, category):
         for player in total_result[category]:
             if player['key'] == player_key:
                 return player
-    except:
+    except KeyError:
         return None
 
     return None
 
 
+def get_best_results(player):
+    results = {}
+
+    for key in player.keys():
+        if key.startswith('score_wt_'):
+            results[key] = player[key]
+
+    result_pairs = list(results.items())
+    result_pairs.sort(key=lambda x: x[1], reverse=False)
+    return [x[0] for x in result_pairs][:7]
+
+
+def calculate_totals(total_result):
+    for category in CATEGORIES:
+        for player in total_result[category]:
+            best_results = get_best_results(player)
+
+            score_wt_total = 0
+            for key in player.keys():
+                if key.startswith('score_wt_'):
+                    if key not in best_results:
+                        player[key] = '[' + str(player[key]) + ']'
+                    else:
+                        score_wt_total += player[key]
+
+            player['score_wt_total'] = score_wt_total
+
+
+
 def calculate_total_score(results):
     total_result = {key: [] for key in CATEGORIES}
 
-    for month in range(1, 2):
+    for month in range(1, 13):
         for category in CATEGORIES:
-            for player in results[month][category]:
-                player_stored = get_player(total_result, player['key'], category)
-                if player_stored is None:
-                    total_result[category].append(
-                        {'key': player['key'], 'name': player['name'], 'score_wt_' + str(month): player['score_wt']})
-                else:
-                    player_stored['score_wt_' + str(month)] = player['score_wt']
+            try:
+                for player in results[month][category]:
+                    player_stored = get_player(total_result, player['key'], category)
+                    if player_stored is None:
+                        total_result[category].append(
+                            {'key': player['key'], 'name': player['name'],
+                             'score_wt_' + str(month): player['score_wt']})
+                    else:
+                        player_stored['score_wt_' + str(month)] = player['score_wt']
+            except KeyError:
+                pass  # there is no results for this month
+
+    calculate_totals(total_result)
+
     return total_result
--- run.py
+++ run.py
@@ -2,10 +2,9 @@ from reader import read
 from calculate import calculate_scores, calculate_total_score
 from publisher import publish
 
-
 if __name__ == "__main__":
     results = dict()
-    for month in range(1, 2): # months from 1 to 12 (jan-dec)
+    for month in range(1, 2):  # months from 1 to 12 (jan-dec)
         players = read(month)
         results[month] = calculate_scores(players)
 
