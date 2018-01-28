4921577a385a71f5ec7fec4ff294d528c432fa15
Federico <fes@foryouandyourcustomers.com>
2018-01-28 02:34:47 +0100

    works with assumptions...

--- calculate.py
+++ calculate.py
@@ -33,25 +33,28 @@ def calculate_scores(players):
     return result
 
 
-def get_player(player_list, player_key):
-    for player in player_list:
-        if player['key'] == player_key:
-            return player
+def get_player(total_result, player_key, category):
+    try:
+
+        for player in total_result[category]:
+            if player['key'] == player_key:
+                return player
+    except:
+        return None
 
     return None
 
 
 def calculate_total_score(results):
-    result = dict()
+    total_result = {key: [] for key in CATEGORIES}
 
-    for month in range(1, 13):
+    for month in range(1, 2):
         for category in CATEGORIES:
             for player in results[month][category]:
-                player_stored = get_player(result[category], player['key'])
-                if player_stored is not None:
-                    result[category].extend(
+                player_stored = get_player(total_result, player['key'], category)
+                if player_stored is None:
+                    total_result[category].append(
                         {'key': player['key'], 'name': player['name'], 'score_wt_' + str(month): player['score_wt']})
                 else:
                     player_stored['score_wt_' + str(month)] = player['score_wt']
-                    result[category][player['key']] = player_stored
-    return result
+    return total_result
--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 from reader import read
 from calculate import calculate_scores, calculate_total_score
-from publisher import publish
+#from publisher import publish
 from pprint import pprint
+
 
 if __name__ == "__main__":
     results = dict()
     for month in range(1, 2): # months from 1 to 12 (jan-dec)
         players = read(month)
         results[month] = calculate_scores(players)
-        import ipdb; ipdb.set_trace()
+        #import ipdb; ipdb.set_trace()
 
     total_results = calculate_total_score(results)
-    publish(total_results, results)
+    pprint(total_results)
+ #   publish(total_results, results)
+    a = 2
