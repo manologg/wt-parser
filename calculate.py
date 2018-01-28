65f0cc5c13b1e63700f520fe89ad476c8adbf900
Federico <fes@foryouandyourcustomers.com>
2018-01-28 01:12:20 +0100

    adds calculation per category

--- calculate.py
+++ calculate.py
@@ -1,7 +1,18 @@
-def add_score(players_per_category):
-    players_per_category = sorted(players_per_category['total'])
+from config import SCORES
 
-    
+
+def add_score(players_per_category):
+    if len(players_per_category) == 0:
+        return players_per_category
+
+    players_per_category.sort(key=lambda x: x['total'], reverse=True)
+
+    players_per_category[0]['score_wt'] = SCORES[0]
+    for i in range(1, len(players_per_category)):
+        if players_per_category[i]['total'] == players_per_category[i - 1]['total']:
+            players_per_category[i]['score_wt'] = players_per_category[i - 1]['score_wt']
+        else:
+            players_per_category[i]['score_wt'] = SCORES[i]
 
     return players_per_category
 
@@ -14,32 +25,10 @@ def calculate_scores(players):
     for category in categories:
         players_per_category = [player for player in players if player['category'] is category]
         for player in players_per_category:
-            player['total'] = player['round_1'] + player['round_2']
+            player['total'] = int(player['round_1']) + int(player['round_2'])
 
         players_per_category = add_score(players_per_category)
 
         result[category] = players_per_category
 
     return result
-
-
-players = [
-    {
-        'category': 'a',
-        'score_wt': 0,
-        'round_1': 30,
-        'round_2': 40
-    }, {
-        'category': 'a',
-        'score_wt': 0,
-        'round_1': 20,
-        'round_2': 30
-    }, {
-        'category': 'a',
-        'score_wt': 0,
-        'round_1': 30,
-        'round_2': 60
-    },
-]
-
-print calculate_scores(players)
