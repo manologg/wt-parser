ac3b28d5ef9e06410bc32512f8f6c29e3d5e591f
Manuel García García <manuel.garcia@zalando.de>
2018-03-12 21:42:16 +0100

    added march results

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-def parse_8_rows(attributes):
+def parse_8_cols(attributes):
 
     category = attributes[4]
     if category not in ['A', 'B', 'C', 'D', 'E']:
@@ -23,22 +23,31 @@ def parse_8_rows(attributes):
 
     return player
 
-
-def parse_9_rows(attributes):
+def parse_9_cols(attributes):
     # 'general_position': attributes[0],
-    return parse_8_rows(attributes[1:])
+    return parse_8_cols(attributes[1:])
+
+def parse_10_cols(attributes):
+    # 'points': attributes[-1],
+    return parse_9_cols(attributes[:-1])
 
 DATA = {
     1: {
         'url': "http://www.frisbee-nrw.de/2018/01/13/dziuba-rockt-den-kletterpark/",
-        'parse_function': parse_8_rows
+        'parse_function': parse_8_cols
     },
     2: {
         'url': "http://www.frisbee-nrw.de/2018/02/10/wt1802-drei-heimsiege-fuer-die-lakers/",
-        'parse_function': parse_9_rows
+        'parse_function': parse_9_cols
     },
-    # 2: ...
-    # 3: ...
+    3: {
+        'url': "http://www.frisbee-nrw.de/2018/03/10/wt1803-ergebnisse/",
+        'parse_function': parse_10_cols
+    },
+    #3: {
+    #    'url': "",
+    #    'parse_function': parse_9_rows
+    #},
 }
 
 SCORES = [30, 26, 23, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
